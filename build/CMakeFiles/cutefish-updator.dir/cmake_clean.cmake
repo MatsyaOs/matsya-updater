d3af2d4c42bb72d6ff08121bde7759e0530deca2
Abhishek Sharma <sabhiron5@gmail.com>
2022-05-15 06:39:08 +0530

    ....

---
+++ build/CMakeFiles/cutefish-updator.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cutefish-updator_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/cutefish-updator_autogen.dir/ParseCache.txt"
+  "cutefish-updator_autogen"
+  "CMakeFiles/cutefish-updator.dir/cutefish-updator_autogen/EWIEGA46WW/qrc_qml.cpp.o"
+  "CMakeFiles/cutefish-updator.dir/cutefish-updator_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/cutefish-updator.dir/main.cpp.o"
+  "CMakeFiles/cutefish-updator.dir/updatorhelper.cpp.o"
+  "CMakeFiles/cutefish-updator.dir/upgradeablemodel.cpp.o"
+  "cutefish-updator"
+  "cutefish-updator.pdb"
+  "cutefish-updator_autogen/EWIEGA46WW/qrc_qml.cpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cutefish-updator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
